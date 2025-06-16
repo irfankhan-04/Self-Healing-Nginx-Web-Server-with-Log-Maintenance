7e2350985101f2919dd3325bb046c2bb64c4981b
irfan khan <er.irfankhan4@gmail.com>
2025-06-16 14:46:11 +0530

    Update delete-old-logs.sh

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+#housekeeping script to delete 7 days old logs
+LOG DIR="/var/log/nginx"
+find "$LOG DIR" -type f -name "*.log" -mtime +7 -exec rm -f [] \;
+echo Old log files deleted from "$LOG DIR"
