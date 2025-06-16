4a4a2934491c9a3779c8bbdf76e2324624fbda59
irfan khan <er.irfankhan4@gmail.com>
2025-06-16 14:49:01 +0530

    Update nginx-monitor.sh

--- nginx-monitor.sh
+++ nginx-monitor.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+#Script to auto-restart Nginx if it's not running.
+PROCESS="nginx"
+if ! pgrep -x "$PROCESS" > /dev/null
+then
+echo "$PROCESS is not running. Restarting.
+sudo systemctl start nginx
+echo "$PROCESS restarted successfully."
+else
+echo "$PROCESS is running fine."
